e0f651a6187a388322718c82ad31d321b08ecef9
Benjamin Salmon <bensalmon8@gmail.com>
2022-10-18 19:58:52 -0500

    fizzbuzz practice

--- practice.js
+++ practice.js
@@ -1,34 +1,13 @@
-// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items.
+// To do so, loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’. Otherwise, say the number.
 
-// For Example:
-// const array1 = ['a', 'b', 'c', 'x']
-// const array2 = ['z', 'y', 'i'];
-// should return false
-// ------------
-// const array1 = ['a', 'b', 'c', 'x']
-// const array2 = ['z', 'y', 'x'];
-// should return true
-
-// 2 parameters - arrays - no size limit
-// return true or false (boolean return)
-
-// const array1 = ["a", "b", "c", "x"];
-// const array2 = ["z", "y", "x"];
-
-// function containsCommonItem(array1, array2) {
-//   for (let i = 0; i < array1.length; i++) {
-//     for (let j = 0; j < array2.length; j++) {
-//       if (array1[i] === array2[j]) return true;
-//     }
-//   }
-//   return false;
-// }
-
-// containsCommonItem(array1, array2);
-
-// O(a * b)
-
-// approach isn't the best because we have two for loops, one nested right after the other.
-
-const array1 = ["a", "b", "c", "x"];
-const array2 = ["z", "y", "x"];
+for (let i = 1; i < 51; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
