4c8ea05326502b94ef1688a0fa4e0499778661db
Benjamin Salmon <bensalmon8@gmail.com>
2022-10-24 16:28:24 -0500

    max of two

--- practice.js
+++ practice.js
@@ -29,18 +29,18 @@
 // For example, if given the array [1, 4, 11, 2, 37, -4], your code should print out -4, 37.
 
 // starting array
-let array = [1, 4, 11, 2, 37, -4];
+// let array = [1, 4, 11, 2, 37, -4];
 
-let smallest = array[0];
-let largest = array[0];
+// let smallest = array[0];
+// let largest = array[0];
 
-for (let i = 0; i < array.length; i++) {
-  if (smallest > array[i]) {
-    smallest = array[0];
-  } else if (largest < array[i]) {
-    largest = array[i];
-  }
-}
+// for (let i = 0; i < array.length; i++) {
+//   if (smallest > array[i]) {
+//     smallest = array[0];
+//   } else if (largest < array[i]) {
+//     largest = array[i];
+//   }
+// }
 
 // let smallest = array[0];
 // let largest = array[0];
@@ -53,4 +53,45 @@ for (let i = 0; i < array.length; i++) {
 //   }
 // }
 
-console.log(smallest, largest);
+// console.log(smallest, largest);
+
+// Given the starting code of:
+
+// x = 1
+// y = 2
+// Write a block of code that will print out the largest value.
+
+// Using the given values as an example, your code should print out 2.
+
+// To verify your code is working properly, try and change the values of x and y.
+
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
+let x = 1;
+let y = 2;
+
+// Write your code below this line.
+
+if (x > y) {
+  console.log(x);
+} else {
+  console.log(y);
+}
+
+// Solution to additional challenge:
+
+if (x > y) {
+  console.log(x);
+} else if (y > x) {
+  console.log(y);
+} else {
+  console.log("The values are identical");
+}
+
+// if (x > y) {
+//   console.log(x)
+// } else if (y > x) {
+//   console.log(y)
+// } else [
+//   console.log('The values are identical.')
+// ]
