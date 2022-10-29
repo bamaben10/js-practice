3b42fb11241369a8e31aae9cd17ca985963e7353
Benjamin Salmon <bensalmon8@gmail.com>
2022-10-29 13:51:58 -0500

    maxoftwo

--- challenge-max-of-two-solution/maxOfTwoSolution.js
+++ challenge-max-of-two-solution/maxOfTwoSolution.js
@@ -1,20 +1,16 @@
-let x = 1
-let y = 2
-
 // Write your code below this line.
-
-if (x > y) {
-  console.log(x)
-} else {
-  console.log(y)
-}
 
 // Solution to additional challenge:
 
-if (x > y) {
-  console.log(x)
-} else if (y > x) {
-  console.log(y)
-} else [
-  console.log('The values are identical.')
-]
+maxOfTwo(1, 2);
+maxOfTwo(3, 3);
+
+function maxOfTwo(x, y) {
+  if (x > y) {
+    console.log(x);
+  } else if (y > x) {
+    console.log(y);
+  } else {
+    console.log("The values are identical.");
+  }
+}
