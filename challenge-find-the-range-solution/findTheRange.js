a48488246af8559615abf033d98356b953392d50
Benjamin Salmon <bensalmon8@gmail.com>
2023-01-27 18:11:43 -0600

    practice

--- challenge-find-the-range-solution/findTheRange.js
+++ challenge-find-the-range-solution/findTheRange.js
@@ -1,16 +1,16 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, -36]
+let array = [28, 43, -12, 30, 4, 0, -36];
 
 // Write your solution below:
-let smallest = array[0]
-let largest = array[0]
+let smallest = array[0];
+let largest = array[0];
 
 for (let i = 0; i < array.length; i++) {
   if (smallest > array[i]) {
-    smallest = array[i]
+    smallest = array[i];
   } else if (largest < array[i]) {
-    largest = array[i]
+    largest = array[i];
   }
 }
 
-console.log(smallest, largest)
+console.log(smallest, largest);
